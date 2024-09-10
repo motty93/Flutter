7e7444ffb1c95b7e30b0d0949deba34039759721
motty93 <rdwbocungelt5@gmail.com>
2024-09-10 16:43:23 +0900

    feat: widget tutorial

---
+++ tutorial/banana/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const app = MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text('banana')),
+    ),
+  );
+
+  runApp(app);
+}
