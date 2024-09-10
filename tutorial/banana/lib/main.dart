11590d2a0d558e23b1e79b24e402081be17db46e
motty93 <rdwbocungelt5@gmail.com>
2024-09-10 17:33:36 +0900

    feat: container

--- tutorial/banana/lib/main.dart
+++ tutorial/banana/lib/main.dart
@@ -9,11 +9,19 @@ void main() {
   // NOTE: memory, fileなどもある
   final img1 = Image.asset('images/food_protein_bar.png');
   final img2 = Image.network('https://flutter-image-network.web.app/inu.jpeg');
+  final con = Container(
+    color: Colors.deepOrange,
+    width: 300,
+    height: 200,
+    alignment: Alignment.centerLeft,
+    padding: const EdgeInsets.all(60),
+    child: img2,
+  );
   final col = Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: [img1, img2]);
+      children: [img1, con]);
   // children: colChildren);
 
   final app = MaterialApp(
