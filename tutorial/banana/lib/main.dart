8f0f08992cd38d0f83a3672ebb403daf992a4e77
motty93 <rdwbocungelt5@gmail.com>
2024-09-10 17:01:16 +0900

    feat: colume/rowについて

--- tutorial/banana/lib/main.dart
+++ tutorial/banana/lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  const colChildren = [
+    Text('レモン'),
+    Text('リンゴ'),
+    Text('ブドウ'),
+  ];
+  const col = Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: colChildren);
+
   const app = MaterialApp(
     home: Scaffold(
-      body: Center(child: Text('banana')),
+      body: Center(child: col),
     ),
   );
 
