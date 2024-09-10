67b76cd87e92d12a39f34b9cc8806546278ef5d1
motty93 <rdwbocungelt5@gmail.com>
2024-09-10 17:19:47 +0900

    feat: Image.asset, Image.network

--- tutorial/banana/lib/main.dart
+++ tutorial/banana/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const colChildren = [
-    Text('レモン'),
-    Text('リンゴ'),
-    Text('ブドウ'),
-  ];
-  const col = Column(
+  // const colChildren = [
+  //   Text('レモン'),
+  //   Text('リンゴ'),
+  //   Text('ブドウ'),
+  // ];
+  // NOTE: memory, fileなどもある
+  final img1 = Image.asset('images/food_protein_bar.png');
+  final img2 = Image.network('https://flutter-image-network.web.app/inu.jpeg');
+  final col = Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: colChildren);
+      children: [img1, img2]);
+  // children: colChildren);
 
-  const app = MaterialApp(
+  final app = MaterialApp(
     home: Scaffold(
       body: Center(child: col),
     ),
